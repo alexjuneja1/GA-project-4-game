1a33fca38f62c92a1cc731fe775350755d3918ff
Alexander Juneja <alexjuneja1@gmail.com>
2015-12-15 11:25:36 -0800

    made a ton of changes, mainly added in the assassin and his functionality as well as some more sprite images. also divided towns and forts into separate variables so i can create different events for each.

--- main.js
+++ main.js
@@ -8,33 +8,37 @@ window.addEventListener("keydown", keydownHandler, false);
 
 //Map code
 var map = [
-  [0, 0, 0, 0, 0, 7],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0]
+  [0, 0, 4, 0, 0, 11],
+  [0, 7, 0, 1, 5, 0],
+  [0, 0, 3, 0, 0, 0],
+  [0, 3, 0, 0, 1, 2],
+  [2, 0, 0, 2, 0, 0],
+  [0, 0, 1, 0, 5, 7]
 ];
 
 var gameObjects = [
   [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 8, 0],
   [0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [8, 0, 0, 0, 0, 0]
+  [12, 0, 0, 0, 0, 0]
 ];
 
 //Map code
 var land = 0;
-var town = 1;
-var enemy1 = 2;
-var enemy2 = 3;
-var enemy3 = 4;
-var enemy4 = 5;
-var enemywander = 6;
-var goal = 7;
-var caravan = 8;
+var town1 = 1;
+var town2 = 2;
+var town3 = 3;
+var enemy1 = 4;
+var enemy2 = 5;
+var enemy3 = 6;
+var enemy4 = 7;
+var assassin = 8;
+var fort1 = 9;
+var fort2 = 10;
+var goal = 11;
+var caravan = 12;
 
 //Size of each cell on the map
 var size = 64;
@@ -52,6 +56,9 @@ var left = 37;
 //Variable and loop to track where the caravan starts at and when the player moves it
 var caravanRow;
 var caravanColumn;
+//Also loop to track where the assassin is at
+var assassinRow;
+var assassinColumn;
 
 for (var row = 0; row < rows; row++) {
   for (var column = 0; column < columns; column++) {
@@ -59,10 +66,20 @@ for (var row = 0; row < rows; row++) {
       caravanRow = row;
       caravanColumn = column;
     }
+    //Render the assassin along with the caravan.
+    if (gameObjects[row][column] === assassin) {
+      assassinRow = row;
+      assassinColumn = column;
+    }
   }
 }
 
 //Game variables
+var food = 10;
+var gold = 10;
+var gameMessage = "Help the Ventlemen complete their quest with the arrow keys!"
+
+render();
 
 function keydownHandler(event) {
   switch(event.keyCode) {
@@ -124,25 +141,135 @@ function keydownHandler(event) {
       gameMessage = "Alex, you need to add the fight code here."
       break;
 
-    case enemywander:
+    case assassin:
       gameMessage = "Alex, you need to add the fight code here."
       break;
 
-    case town:
+    case town1:
       gameMessage = "Alex, you need to add the town code here."
+      break;
+
+    case town2:
+      gameMessage = "Alex, you need to add the town code here."
+      break;
+
+    case town3:
+      gameMessage = "Alex, you need to add the town code here."
+      break;
+
+    case fort:
+      gameMessage = "Alex, you need to add the castle code here."
       break;
 
     case goal:
       gameMessage = "Alex, you need to add the ending here."
       break;
   }
-  //Do things to the game variables here
+  //Subtract food by 1 each turn
+  food--;
+  //Check if the caravan has run out of food
+  if (food <= 0) {
+    console.log("The end game function goes here.")
+  }
+  //Move the assassin with each keypress
+  moveAssassin();
 
   //Re-render the game at the end of a keypress
   render();
 }
 
-render();
+function moveAssassin() {
+  //The 4 possible directions that the monster can move
+  var UP = 1;
+  var DOWN = 2;
+  var LEFT = 3;
+  var RIGHT = 4;
+
+  //An array to store the valid direction that the monster is allowed to move in
+  var validDirections = [];
+
+  //The final direction that the monster will move in
+  var direction = undefined;
+
+  //Find out what kinds of things are in the cells that surround the assassin
+  //If the cells contain land, push the corresponding direction into the validDirections array
+  if (assassinRow > 0) {
+    var thingAbove = map[assassinRow - 1][assassinColumn];
+    if (thingAbove === land) {
+      validDirections.push(UP)
+    }
+  }
+  if (assassinRow < rows - 1) {
+    var thingBelow = map[assassinRow + 1][assassinColumn];
+    if (thingBelow === land) {
+      validDirections.push(DOWN)
+    }
+  }
+  if (assassinColumn > 0) {
+    var thingToTheLeft = map[assassinRow][assassinColumn - 1];
+    if (thingToTheLeft === land) {
+      validDirections.push(LEFT)
+    }
+  }
+  if (assassinColumn < columns - 1) {
+    var thingToTheRight = map[assassinRow][assassinColumn + 1];
+    if (thingToTheRight === land) {
+      validDirections.push(RIGHT)
+    }
+  }
+  //If a valid direction was found, randomly choose one of the possible directions
+  if (validDirections.length !== 0) {
+    var randomNumber = Math.floor(Math.random() * validDirections.length);
+    direction = validDirections[randomNumber];
+  }
+  //Move the assassin in the chosen direction
+  switch(direction) {
+    case UP:
+      //Clear assassin's current cell
+      gameObjects[assassinRow][assassinColumn] = 0;
+      //Subtract 1 from the assassin's row
+      assassinRow--;
+      //Apply the assassin's new updated position into the array
+      gameObjects[assassinRow][assassinColumn] = assassin;
+      break;
+
+    case DOWN:
+      gameObjects[assassinRow][assassinColumn] = 0;
+      assassinRow++;
+      gameObjects[assassinRow][assassinColumn] = assassin;
+      break;
+
+    case LEFT:
+      gameObjects[assassinRow][assassinColumn] = 0;
+      assassinColumn--;
+      gameObjects[assassinRow][assassinColumn] = assassin;
+      break;
+
+    case RIGHT:
+      gameObjects[assassinRow][assassinColumn] = 0;
+      assassinColumn++;
+      //Apply the assassin's new updated position into the array
+      gameObjects[assassinRow][assassinColumn] = assassin;
+      break;
+  }
+}
+
+function endGame() {
+  if(map[caravanRow][caravanColumn] === goal) {
+    // Calculate a score
+    var score = food + gold;
+    console.log("You made it home! " + "Final Score: " + score);
+    //Remove the event listener to end the game, then add some way to return to a title overlay
+    window.removeEventListener("keydown", keydownHandler, false);
+  }
+  else {
+    if (food <= 0) {
+      gameMessage += " You have run out of food and your party has perished...";
+      //Remove the event listener to end the game, then add some way to return to a title overlay
+      window.removeEventListener("keydown", keydownHandler, false);
+    }
+  }
+}
 
 function render() {
   //Clearing the stage of img tag cells from the previous turn
@@ -163,7 +290,7 @@ function render() {
       //Applying correct image to the map cell
       switch(map[row][column]) {
         case land:
-          // cell.src = image source;
+          //cell.src = image source;
           break;
 
         case town:
@@ -186,8 +313,12 @@ function render() {
           cell.src = "enemy4.png";
           break;
 
-        case enemywander:
+        case assassin:
           cell.src = "enemywander.png";
+          break;
+
+        case fort:
+          cell.src = "castle.png";
           break;
 
         case goal:
@@ -197,7 +328,7 @@ function render() {
       //Add the caravan from the gameObjects array
       switch(gameObjects[row][column]) {
         case caravan:
-        // cell.src = image source;
+        cell.src = "caravan.png"
         break;
       }
       //Position the cell
